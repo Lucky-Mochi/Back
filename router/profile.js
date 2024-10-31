fe841e71f481cef9036ef59dacc424c19e6472f0
heondolee <heondo999@inu.ac.kr>
2024-11-01 03:35:29 +0900

    멘토 후기 작성 완료

--- router/profile.js
+++ router/profile.js
@@ -181,5 +181,49 @@ router.delete("/mentoring", async (req, res) => {
   }
 });
 
+router.post("/review", async (req, res) => {
+  try {
+    const token = req.headers["authorization"];
+    const tokenValue = token ? token.split(" ")[1] : null;
+
+    // 토큰이 없거나 유효하지 않은 경우 처리
+    if (!tokenValue) {
+      return res.status(400).json({ success: false, message: "토큰이 제공되지 않았습니다." });
+    }
+
+    const user = await User.findOne({ where: { accessToken: tokenValue } });
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: "유효하지 않은 토큰입니다." });
+    }
+
+    const {
+      mentoId,
+      temperature, // rating을 temperature로 변경
+    } = req.body;
+
+    // 필수 입력 값 확인
+    if (!mentoId || !temperature) {
+      return res.status(400).json({ success: false, message: "필수 입력 값이 없습니다." });
+    }
+
+    // 리뷰의 유효성 체크 (1 ~ 5)
+    if (temperature < 1 || temperature > 5) {
+      return res.status(400).json({ success: false, message: "온도는 1에서 5 사이여야 합니다." });
+    }
+
+    await User.update({
+      temperature
+    }, {
+      where: { id: mentoId }
+    });
+
+    return res.status(200).json({ success: true, message: "멘토/멘티의 후기가 저장되었습니다." });
+
+  } catch (error) {
+    console.error("리뷰 작성 에러:", error);
+    res.status(500).json({ success: false, message: "서버 에러가 발생했습니다." });
+  }
+});
 
 module.exports = router;
